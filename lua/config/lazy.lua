ed415ff85882ca1e3064284c45877b5db52beaef
Neil Li <76211576+neil1050@users.noreply.github.com>
2026-06-27 21:07:50 +0100

    feat: add plugins for nvim cmp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -46,6 +46,38 @@ require("lazy").setup({
     {
         "L3MON4D3/LuaSnip",
         lazy = false,
+    },
+    {
+        "saadparwaiz1/cmp_luasnip",
+        dependencies = {
+            "L3MON4D3/LuaSnip"
+        },
+        lazy = false,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+    },
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        lazy = false,
+        opts = {}
+    },
+    {
+        "hrsh7th/cmp-buffer",
+        lazy = false,
+    },
+    {
+        "hrsh7th/cmp-path",
+        lazy = false,
+    },
+    {
+        "hrsh7th/cmp-cmdline",
+        lazy = false,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        lazy = false,
         opts = {}
     },
     -- import your plugins
