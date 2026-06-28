ed39cf25a041c9fb18f7d4cdc9e2d06fc0f088a9
Neil Li <76211576+neil1050@users.noreply.github.com>
2026-06-28 17:08:05 +0100

    feat: add mason package manager to plugins
    this makes language server management easier

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+-- setup mason.nvim and mason-lspconfig
+return {
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            {"mason-org/mason.nvim", opts = {}},
+            "neovim/nvim-lspconfig",
+        }
+    }
+}
