c69bea5b8325d78bd80540ee896011804de543db
Neil Li <76211576+neil1050@users.noreply.github.com>
2026-06-28 17:28:35 +0100

    fix: ensure vimwiki only loads with a wiki file

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -2,6 +2,7 @@
 return {
     {
         "vimwiki/vimwiki",
+        ft = "wiki",
         init = function()
             vim.g.vimwiki_path = "~/vimwiki/"
             vim.g.vimwiki_syntax = "default"
