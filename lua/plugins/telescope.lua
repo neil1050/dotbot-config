9fbcf72db3935c4487aa026803c5dabd78ec255c
Neil Li <76211576+neil1050@users.noreply.github.com>
2026-07-02 21:27:29 +0100

    fix(telescope.nvim): add exclusions to certain paths

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,14 @@ return {
             -- optional but recommended
             -- C-make and C++ build tools not installed, cannot use
             -- {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
-        }
+        },
+        -- if ripgrep not installed
+        --[[opts = {
+            defaults = {
+                file_ignore_patterns = {
+                    ".venv", ".git"
+                }
+            },
+        },]]
     }
 }
