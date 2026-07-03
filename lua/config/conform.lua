751a6ab1b583085699d2dc214541c0fc4e580ad7
Neil Li <76211576+neil1050@users.noreply.github.com>
2026-07-03 21:34:23 +0100

    fix: allow formatter to use async processing
    this prevents the timeout error from black being slow for some reason

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
     callback = function (args)
-       require("conform").format({bufnr = args.buf})
+       require("conform").format({async = true, bufnr = args.buf})
     end})
