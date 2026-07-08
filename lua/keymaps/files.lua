ed7e05ef16ae90832f8d4ba8cf944fc22c0e07fa
Neil Li <76211576+neil1050@users.noreply.github.com>
2026-07-08 21:44:05 +0100

    feat(keymaps): add keymaps for saving and reopening a file
    Signed-off-by: Neil Li <76211576+neil1050@users.noreply.github.com>

--- lua/keymaps/files.lua
+++ lua/keymaps/files.lua
@@ -1,1 +1,7 @@
+-- telescope
 vim.keymap.set("n", "<Leader>ff", "<cmd>Telescope find_files<CR>", {desc = "telescope find_files", silent = true})
+
+-- basic file management
+vim.keymap.set("n", "<Leader>fw", "<cmd>w<cr>", {desc = "write file"})
+-- discards unsaved changes
+vim.keymap.set("n", "<Leader>fr", "<cmd>e! %<cr>", {desc = "reopen file"})
