a11f43966fd8a1be0f8b2dd0e5586138077b8db7
Neil Li <76211576+neil1050@users.noreply.github.com>
2026-07-08 21:36:48 +0100

    fix(keymaps): ensure keymaps use cmd instead of colon
    Signed-off-by: Neil Li <76211576+neil1050@users.noreply.github.com>

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>", {desc = "telescope find_files", silent = true})
+vim.keymap.set("n", "<Leader>ff", "<cmd>Telescope find_files<CR>", {desc = "telescope find_files", silent = true})
